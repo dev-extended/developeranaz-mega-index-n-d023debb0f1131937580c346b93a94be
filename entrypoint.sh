de03bd2697343dbe18f4895b1bd3b4ee0da6ebbb
developeranaz_BOT <96877776+dev-extended@users.noreply.github.com>
2022-10-03 14:09:54 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,6 +12,11 @@ touch /Mega-index-heroku/quota-bypass/checkquota.log
 /home/$RCR version
 /home/$RCR config create 'CLOUDNAME' 'mega' 'user' $UserName 'pass' $PassWord
 /home/$RCR version
+
+
+if [ "$Auto_Quota_Bypass" = true ] ; then
+
+
 /home/$RCR serve http CLOUDNAME: --addr :$PORT --buffer-size 256M --dir-cache-time 12h --vfs-read-chunk-size 256M --vfs-read-chunk-size-limit 2G --vfs-cache-mode writes -v > "$log" 2>&1 &
 while sleep 20
 do
@@ -23,3 +28,9 @@ do
 done
 
 #Bandwidth Limit Exceeded
+
+else 
+
+/home/$RCR serve http CLOUDNAME: --addr :$PORT --buffer-size 256M --dir-cache-time 12h --vfs-read-chunk-size 256M --vfs-read-chunk-size-limit 2G --vfs-cache-mode writes
+
+fi

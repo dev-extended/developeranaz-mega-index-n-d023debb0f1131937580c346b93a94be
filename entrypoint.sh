9f9b21f56eaadc8300b990969e7afaa07cb75919
developeranaz_BOT <96877776+dev-extended@users.noreply.github.com>
2022-10-03 14:01:42 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,7 +15,7 @@ touch /Mega-index-heroku/quota-bypass/checkquota.log
 /home/$RCR serve http CLOUDNAME: --addr :$PORT --buffer-size 256M --dir-cache-time 12h --vfs-read-chunk-size 256M --vfs-read-chunk-size-limit 2G --vfs-cache-mode writes -v > "$log" 2>&1 &
 while sleep 20
 do
-    if fgrep --quiet "s" "$log"
+    if fgrep --quiet "Bandwidth Limit Exceeded" "$log"
     then
         cd /Mega-index-heroku/quota-bypass
         bash bypass.sh
